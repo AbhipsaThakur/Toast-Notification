46a54347be7594ae30b1053b9654bc51c6bcf593
AbhipsaThakur <abhipsathakur933@gmail.com>
2025-05-24 16:35:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function showToast(type = 'info', message = '') {
-  const toast = document.createElement('div');
-  toast.classList.add('toast', type);
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class= "fa-solid fa-circle-xmark"></i> Please fix the error!';
+let invalidMsg = '<i class = "fa-solid fa-circle-exclamation"></i>Invalid input, check again';
+function showToast(msg) {
+  let toast = document.createElement('div');
+  toast.classList.add('toast');
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
 
-  toast.innerHTML = `
-    <span>${message}</span>
-    <span class="close" onclick="this.parentElement.remove()">×</span>
-  `;
-
-  document.getElementById('toast-container').appendChild(toast);
-
-  // Auto remove after 4 seconds
-  setTimeout(() => {
+  if(msg.includes('error')){
+    toast.classList.add('error');
+  }
+  if(msg.includes('Invalid')){
+    toast.classList.add('invalid');
+  }
+  setTimeout(()=>{
     toast.remove();
-  }, 4000);
+  },6000);
 }
